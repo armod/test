32e399aadc11d4220348a2ef32097cd625f21c8c
armod <armods@gmail.com>
2022-08-24 21:51:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Komp from './Komp';
+
+function App() {
+  return (
+    <div className="App">
+      <Komp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Komp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Komp extends Component {
+  render() {
+    return (
+      <div>Komponent</div>
+    )
+  }
+}
